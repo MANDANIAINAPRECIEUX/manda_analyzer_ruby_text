d73e74bcc30bbd3c23e3e206d593bd47578841a3
unknown <mandaniainaprecieux@yahoo.com>
2025-12-17 13:10:18 +0300

    Add search feature with occurrences and line numbers

--- analyzer.rb
+++ analyzer.rb
@@ -1,3 +1,16 @@
+search_term = nil
+
+if ARGV.include?("--search")
+  index = ARGV.index("--search")
+  search_term = ARGV[index + 1]
+end
+if ARGV.include?("--search") && search_term.nil?
+  puts "Erreur: veuillez fournir un mot ou une phrase après --search"
+  exit
+end
+
+
+
 if ARGV.empty?
     puts "Usage : ruby analyzer.rb <fichier.txt>"
     exit
@@ -8,15 +21,26 @@ unless File.exist? (file_path)
     puts "Erreur : le fichier '#{file_path}' n'existe pas. "
     exit
 end
+
  lines_count = 0
  words_count = 0
  chars_count = 0
+ occurrences = 0
+matching_lines = []
 
- File.foreach (file_path) do |line|
-    lines_count +=1
-    words_count += line.split.size
-    chars_count = line.length
- end
+ File.foreach(file_path).with_index(1) do |line, line_number|
+  lines_count += 1
+  words_count += line.split.size
+  chars_count += line.length
+
+  if search_term
+    if line.downcase.include?(search_term.downcase)
+      occurrences += line.downcase.scan(search_term.downcase).size
+      matching_lines << [line_number, line.strip]
+    end
+  end
+end
+
 
 
  puts "File : #{file_path}"
@@ -25,3 +49,18 @@ end
  puts " - Lines : #{lines_count}"
  puts " - Words : #{words_count}"
  puts " - Characters : #{chars_count}"
+ if search_term
+  puts
+  puts "Search: \"#{search_term}\""
+  puts "Occurrences: #{occurrences}"
+
+  if matching_lines.any?
+    puts
+    puts "Lines:"
+    matching_lines.each do |line_number, content|
+      puts "  #{line_number}: #{content}"
+    end
+  else
+    puts "No matches found."
+  end
+end
